f6da9f73be64e28a80b475b8b276c1dbec8322ef
Plamen Petrov <133807832+driverstudio89@users.noreply.github.com>
2023-05-28 17:43:05 +0300

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    private static final String ROCK = "Rock";
+    private static final String PAPER = "Paper";
+    private static final String SCISSORS = "Scissors";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Choose [r]ock, [p]aper or [s]cissors: ");
+
+        String playerMove = scanner.nextLine();
+
+        if (playerMove.equals("r") || playerMove.equals("rock")) {
+            playerMove = ROCK;
+        } else if (playerMove.equals("p") || playerMove.equals("paper")) {
+            playerMove = PAPER;
+        } else if (playerMove.equals("s") || playerMove.equals("scissors")) {
+            playerMove = SCISSORS;
+        } else {
+            System.out.println("Invalid Input. Try Again...");
+            return;
+        }
+
+        Random random = new Random();
+        int computerRandomNumber = random.nextInt(3);
+
+        String computerMove = "";
+        switch (computerRandomNumber) {
+            case 0:
+                computerMove = "Rock";
+                break;
+            case 1:
+                computerMove = "Paper";
+                break;
+            case 2:
+                computerMove = "Scissors";
+                break;
+        }
+        System.out.printf("The computer chose %s.%n", computerMove);
+
+        if ((playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) ||
+                (playerMove.equals(PAPER) && computerMove.equals(ROCK)) ||
+                (playerMove.equals(SCISSORS) && computerMove.equals(PAPER))) {
+            System.out.println("You win.");
+        } else if ((playerMove.equals(ROCK) && computerMove.equals(PAPER)) ||
+                (playerMove.equals(PAPER) && computerMove.equals(SCISSORS)) ||
+                (playerMove.equals(SCISSORS) && computerMove.equals(ROCK))) {
+            System.out.println("You lose");
+        } else if ((playerMove.equals(ROCK) && computerMove.equals(ROCK)) ||
+                (playerMove.equals(PAPER) && computerMove.equals(PAPER)) ||
+                (playerMove.equals(SCISSORS) && computerMove.equals(SCISSORS))) {
+            System.out.println("This game was a draw");
+        }
+
+    }
+
+
+}
